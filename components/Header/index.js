0d679a746037f589ab4a261d80e76b9c7d91dde8
Jackie Atwood <takalaj@verizon.net>
2019-08-09 14:03:54 -0400

    ...........

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,27 @@
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
-function Header() {}
+const header = document.querySelector(".header-container");
+console.log(header)
+
+function createHeader() {
+    const header = document.createElement("div");
+    const date = document.createElement("span");
+    const times = document.createElement("h1");
+    const temp = document.createElement("span");
+
+    header.appendChild(date);
+    header.appendChild(times);
+    header.appendChild(temp);
+
+    header.classList.add("header");
+    date.classList.add("date");
+    temp.classList.add("temp");
+
+    date.textContent = (`SMARCH 28, 2019`);
+    times.textContent = (`Lambda Times`);
+    temp.textContent = (`98*`);
+  //do I need to hardcode or access an array with text content.
+
+    return header;
+}
